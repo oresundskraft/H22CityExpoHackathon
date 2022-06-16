bdc75f3d50bb8256db8f4e427be27faf0e8c6131
Anil Kumar Kothapalli <anilkumar1988@gmail.com>
2022-06-16 11:17:08 +0200

    minor changes

--- .streamlit/pages/1_Parking.py
+++ .streamlit/pages/1_Parking.py
@@ -77,10 +77,12 @@ fig.update_layout(mapbox_style="open-street-map")
 fig.update_traces(marker={'size': 15,'opacity':0.8})
 if address_search:
     fig.add_trace(go.Scattermapbox(
-        #mode = "markers",
-        lon = [float(filtered_address['lng'])],
-        lat = [float(filtered_address['lat'])],
-        marker = {'size': 15}))
+        lon = [float(filtered_address['lng']) ],
+        lat = [float(filtered_address['lat']) ],
+        hovertext=filtered_address['Adress'].values[0],  
+        hoverinfo='text',                            
+        marker=dict(size=20, color='black'))         
+                  )
 
 st.plotly_chart(fig)
 
